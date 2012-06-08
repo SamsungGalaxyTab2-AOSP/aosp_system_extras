e931b5abc1355597d7e66f7cb82a35a70e8ec72c
David Ferguson <ferguson.david@gmail.com>
2012-06-07 21:00:41 -0400

    ext4_utils: add BoardConfig define to suppress EMMC-corrupting wipe command
    If BOARD_SUPPRESS_EMMC_WIPE is true, the EMMC wipe command will not be
    issued. This works around a bug in some firmware revisions of Samsung EMMC's
    that permanently damages the device when the wipe command is issued.
    
    For affected devices with kernel source, MMC_CAP_ERASE should be removed
    from the kernel instead.
    
    This is only part of the solution but it does handle the "flashing CM9
    for the first time on an unsafe kernel" situation.
    
    Change-Id: Ie4e31f9268a65218e5d344ae3068b021790fc33c

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -61,6 +61,11 @@ LOCAL_SHARED_LIBRARIES := \
     libselinux \
     libsparse \
     libz
+
+ifeq ($(BOARD_SUPPRESS_EMMC_WIPE),true)
+    LOCAL_CFLAGS += -DSUPPRESS_EMMC_WIPE
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -70,6 +75,11 @@ LOCAL_MODULE := libext4_utils_static
 LOCAL_STATIC_LIBRARIES += \
     libselinux \
     libsparse_static
+
+ifeq ($(BOARD_SUPPRESS_EMMC_WIPE),true)
+    LOCAL_CFLAGS += -DSUPPRESS_EMMC_WIPE
+endif
+
 include $(BUILD_STATIC_LIBRARY)
 
 
